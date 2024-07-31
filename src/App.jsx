371bf2d4a932e041309f55b64124ea1e9b947848
Ali <2153970@cognizant.com>
2024-07-31 12:11:18 +0530

    revenue card commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { RevenueCard } from './components/RevenueCard'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className='grid grid-cols-3'>
+      <RevenueCard title={"Amount Pending"} amount={"92,312,.20"} orderCount={13}/>
+    </div>
   )
 }
 

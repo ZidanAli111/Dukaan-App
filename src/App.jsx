d8206de56825613d0240b6256cc850aec3305663
Ali <2153970@cognizant.com>
2024-09-06 11:38:35 +0530

    final code

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,24 @@ import { NextPayoutCard } from './components/NextPayoutCard'
 import { Overview } from './components/Overview'
 import { PaymentHeader } from './components/PaymentHeader'
 import { RevenueCard } from './components/RevenueCard'
-import { Statistics } from './components/Statistics'
 import { Transactions } from './components/Transactions'
 import { TransactionsStatus } from './components/TransactionsStatus'
 
 function App() {
   return (
-    <div>
-      {/* <PaymentHeader></PaymentHeader>
+    <div className=''>
+      <PaymentHeader></PaymentHeader>
       <main className=' p-3 sm:p-8 w-full gap-8 flex flex-col'>
         <Overview></Overview>
+        <div className='flex   gap-2 '>
         <NextPayoutCard title={"Next Payout"} amount={"2,312,.23"} orderCount={23} orderDate={"Today, 4:00PM"} />
         <RevenueCard title={"Amount Pending"} amount={"92,312.20"} orderCount={23} />
         <AmountProceed title={"Amount Processed"} amount={"23,92,312.19"} />
+        </div>
         <Transactions></Transactions>
         <TransactionsStatus></TransactionsStatus>
-      </main> */}
-      <Statistics></Statistics>
+      </main>
+
     </div>
   )
 }
